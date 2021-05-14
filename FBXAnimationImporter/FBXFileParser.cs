8081d2a5053c9c2c790f188a4ebb75dcf7e56085
ShoosGun <ivanrwpf@gmail.com>
2021-05-14 16:27:45 -0300

    Made all the file parsing happen all in one class

--- FBXAnimationImporter/FBXFileParser.cs
+++ FBXAnimationImporter/FBXFileParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FBXRuntimeImporter
@@ -21,5 +22,41 @@ namespace FBXRuntimeImporter
 
         //EX: 7400 (7.4)
         public int FileVersion;
+
+        public FBXFileParser(string filePath)
+        {
+            if (!filePath.EndsWith(".fbx"))
+                throw new Exception("Incorrect File Type");
+
+            List<byte> fbxFile = new List<byte>(File.ReadAllBytes(filePath));
+            FileVersion = BitConverter.ToInt32(fbxFile.GetRange(23, 26).ToArray(), 0);
+
+            FBXHeaderExtension = new FBXRecordNode(ref fbxFile, 27);
+            FileId = new FBXRecordNode(ref fbxFile, (int)FBXHeaderExtension.EndOffset);
+            CreationTime = new FBXRecordNode(ref fbxFile, (int)FileId.EndOffset);
+            Creator = new FBXRecordNode(ref fbxFile, (int)CreationTime.EndOffset);
+            GlobalSettings = new FBXRecordNode(ref fbxFile, (int)Creator.EndOffset);
+            Documents = new FBXRecordNode(ref fbxFile, (int)GlobalSettings.EndOffset);
+            References = new FBXRecordNode(ref fbxFile, (int)Documents.EndOffset);
+            Definitions = new FBXRecordNode(ref fbxFile, (int)References.EndOffset);
+            Objects = new FBXRecordNode(ref fbxFile, (int)Definitions.EndOffset);
+            Connections = new FBXRecordNode(ref fbxFile, (int)Objects.EndOffset);
+            Takes = new FBXRecordNode(ref fbxFile, (int)Connections.EndOffset);
+        }
+
+        public string AllNodesToString()
+        {
+            return FBXHeaderExtension.ToString()
+                    + "\n\n" + FileId.ToString()
+                    + "\n\n" + CreationTime.ToString()
+                    + "\n\n" + Creator.ToString()
+                    + "\n\n" + GlobalSettings.ToString()
+                    + "\n\n" + Documents.ToString()
+                    + "\n\n" + References.ToString()
+                    + "\n\n" + Definitions.ToString()
+                    + "\n\n" + Objects.ToString()
+                    + "\n\n" + Connections.ToString()
+                    + "\n\n" + Takes.ToString();
+        }
     }
 }
--- FBXAnimationImporter/Program.cs
+++ FBXAnimationImporter/Program.cs
@@ -19,54 +19,10 @@ namespace FBXRuntimeImporter
                 filePath = Console.ReadLine();
             }
 
-            List<byte> fbxFile;
-            try{
-                fbxFile = new List<byte>(File.ReadAllBytes(filePath));
-            }
-            catch{
-                Console.WriteLine("Nao deu para ler o arquivo");
-                Console.Read();
-                return;
-            }
+            FBXFileParser fileParser = new FBXFileParser(filePath);
 
-            uint fileVersion = BitConverter.ToUInt32(fbxFile.GetRange(23,26).ToArray(),0);
-            Console.WriteLine("Versao do Arquivo: " + fileVersion);
-            Console.WriteLine("Tamanho do Arquivo: " + fbxFile.Count);
-
-            const ulong firstByteAfterTheHeader = 27; //O Header vai de 0 - 26, então começamos no 27
-
-            List<FBXRecordNode> AllRecordNodes = new List<FBXRecordNode>() {new FBXRecordNode(ref fbxFile,(int)firstByteAfterTheHeader) };
-
-            for(int i =0; i<10; i++)
-                    AllRecordNodes.Add(new FBXRecordNode(ref fbxFile, (int)AllRecordNodes[AllRecordNodes.Count - 1].EndOffset));
-               
-
-            //AllRecordNodes.RemoveAt(AllRecordNodes.Count - 1);
-            
-            foreach(var node in AllRecordNodes) { 
-            Console.Write("/\n/\n/\n"); //Parede de / / /
-            Console.WriteLine(node);//8th node is the object node, where animations, geometry and materials are stored
-            }
-            //Position & Angles are stored in 3D
-            List<FBXAnimationNodes> animations = new List<FBXAnimationNodes>();
-            int animToGiveNodes = -1; //1- - none, 0 - animationOne, 1 - //Two
-            int nodeToGiveCurve = -1;
-            for (int i = 0; i < AllRecordNodes[8].NestedRecords.Count; i++)
-            {
-                if (AllRecordNodes[8].NestedRecords[i].Name == "AnimationStack" && AllRecordNodes[8].NestedRecords[i + 1].Name == "AnimationLayer")
-                {
-                    animToGiveNodes++;
-                    nodeToGiveCurve = -1;
-                    animations.Add(new FBXAnimationNodes(AllRecordNodes[8].NestedRecords[i], AllRecordNodes[8].NestedRecords[i + 1]));
-                }
-                else if (AllRecordNodes[8].NestedRecords[i].Name == "AnimationCurveNode")
-                {
-                    nodeToGiveCurve++;
-                    animations[animToGiveNodes].AnimationNodes.Add(new AnimationNode(AllRecordNodes[8].NestedRecords[i]));
-                }
-                else if (animToGiveNodes > -1 && animToGiveNodes < animations.Count && AllRecordNodes[8].NestedRecords[i].Name == "AnimationCurve")
-                    animations[animToGiveNodes].AnimationNodes[nodeToGiveCurve].AnimationCurves.Add(AllRecordNodes[8].NestedRecords[i]);
-            }
+            Console.WriteLine("Versao do Arquivo: " + fileParser.FileVersion);
+            Console.WriteLine(fileParser. AllNodesToString());
             ////AnimationLayer and then AnimationStack indicates the start of a new animation, and AnimationNodes and AnimationCurves
             ////the curve inside the animation, where the curves of the bones are in order, with rotation first and then position curves		
             ////the AnimationNodes/AnimationCurves array ending means that there are no more curves for that animation
